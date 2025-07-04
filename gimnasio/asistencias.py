d730e7e651988bf6f3f4127d6865cad6c65d01ca
Diego052508 <dfarmengol@uamv.edu.ni>
2025-07-03 21:01:18 -0600

    Update asistencias.py

--- gimnasio/asistencias.py
+++ gimnasio/asistencias.py
@@ -4,15 +4,15 @@ from datos import clientes, asistencias, guardar_datos
 # Registra la asistencia de un cliente si está activo
 def registrar_asistencia():
     cedula = input("Cédula del cliente: ")
-    if cedula not in clientes:
+    if cedula not in clientes: #Busca al cliente con la cedula introducida
         print("Cliente no encontrado.")
         return
 
     cliente = clientes[cedula]
     if not cliente['activo']:
-        print("Cliente con membresía vencida o inactiva.")
+        print("Cliente con membresía vencida o inactiva.") # si el cliente no esta activo no puede marcar asistencia
         return
 
     asistencias.append({'cedula': cedula, 'fecha': datetime.now()})
-    guardar_datos()
+    guardar_datos() #se guarda la asitencia
     print(f"\nAsistencia registrada para {cliente['nombre']}.\n")
